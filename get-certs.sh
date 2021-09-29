f785c67c9eee13ae41270c196f61cc922dd35f73
Nate Fickas <nfickas15@gmail.com>
2021-09-29 14:38:22 -0500

    update namespaces

--- get-certs.sh
+++ get-certs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-name=$(kubectl get pod -n nate-test -o custom-columns=":metadata.name" -l postgres-operator.crunchydata.com/cluster=hippo,postgres-operator.crunchydata.com/role=master)
+name=$(kubectl get pod -n gisdemo -o custom-columns=":metadata.name" -l postgres-operator.crunchydata.com/cluster=hippo,postgres-operator.crunchydata.com/role=master)
 name_2=${name%-*}
-kubectl get secrets -n nate-test $name_2-certs -o go-template='{{index .data "patroni.ca-roots" | base64decode}}'
-kubectl get secrets -n nate-test $name_2-certs -o go-template='{{index .data "dns.crt" | base64decode}}'
-kubectl get secrets -n nate-test $name_2-certs -o go-template='{{index .data "dns.key" | base64decode}}'
+kubectl get secrets -n gisdemo $name_2-certs -o go-template='{{index .data "patroni.ca-roots" | base64decode}}'
+kubectl get secrets -n gisdemo $name_2-certs -o go-template='{{index .data "dns.crt" | base64decode}}'
+kubectl get secrets -n gisdemo $name_2-certs -o go-template='{{index .data "dns.key" | base64decode}}'
 echo "Grafana Password: "
-kubectl get secrets -n nate-test hippo-pguser-grafana -o go-template='{{index .data "password" | base64decode}}'
+kubectl get secrets -n gisdemo hippo-pguser-grafana -o go-template='{{index .data "password" | base64decode}}'
