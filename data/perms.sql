e6de9855f6b1d93be47b2e17a632cf2a49c89bfb
Nate Fickas <nfickas15@gmail.com>
2021-10-06 14:23:01 -0500

    Fix perms for no grafana

--- data/perms.sql
+++ data/perms.sql
@@ -1,27 +1,9 @@
-GRANT SELECT ON fire_hazard_areas TO tileserv;
 GRANT SELECT ON county_boundary TO tileserv;
 GRANT SELECT ON fire_hazard_areas TO featureserv;
 GRANT SELECT ON fire_district_sphere_of_influence TO featureserv;
 CREATE SCHEMA IF NOT EXISTS postgisftw;
-CREATE TABLE fires (gid serial PRIMARY KEY, geom geometry(POINT, 4326));
 GRANT SELECT ON fires to featureserv;
 GRANT INSERT ON fires to featureserv;
 GRANT SELECT ON fires_gid_seq to featureserv;
 GRANT USAGE ON fires_gid_seq to featureserv;
 GRANT USAGE ON SCHEMA postgisftw TO featureserv;
-GRANT USAGE ON SCHEMA postgisftw TO grafana;
-GRANT SELECT ON county_boundary to grafana;
-GRANT SELECT ON facilities to grafana;
-GRANT SELECT ON fires to grafana;
-
-ALTER TABLE county_boundary
-ADD COLUMN time timestamp;
-
-UPDATE county_boundary
-SET time = current_timestamp;
-
-ALTER TABLE facilities
-ADD COLUMN time timestamp;
-
-UPDATE facilities
-SET time = current_timestamp;
